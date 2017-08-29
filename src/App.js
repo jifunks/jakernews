bfdab4ee3bd52c787af068022d3af6a6c05d4614
Jake Funke <jifunks@gmail.com>
2017-08-29 11:37:24 -0700

    end ch1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// create artificial data that we will model realistically later
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+]
+
+// App extends from Component (inheritance)
+// Components have useful methods like render() etc. Building block for a React app.
+
 class App extends Component {
   render() {
-    const helloWorld = 'Welcome to the Road to learn React';
-    //className used in JSX instead of class
-    const name = {'first':"Jake", 'last':'Funke'}
     return (
-      <div className="App">
-          <h2>{helloWorld}</h2>
-          <p>Hello Mr. {name.last}. Or {name.first} I guess. Welcome to your site.</p>
-      </div>
+        <div className="App">
+          { list.map(function(item) {
+            return(
+              <div key={item.objectID}>
+                <span>
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+              </div>
+              )
+          })}
+        </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,15 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
+
 registerServiceWorker();
+
+// lets react app reload without a page refresh (!)
+// this is amazing.
+if (module.hot) {
+    module.hot.accept()
+}
