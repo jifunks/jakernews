4fef50e6b103037840cd90fabe0f0064687589d6
Jake Funke <jifunks@gmail.com>
2017-08-30 16:35:59 -0700

    Added Standard linting style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 // create artificial data that we will model realistically later
 const list = [
@@ -9,7 +9,7 @@ const list = [
     author: 'Jordan Walke',
     num_comments: 3,
     points: 4,
-    objectID: 0,
+    objectID: 0
   },
   {
     title: 'Redux',
@@ -17,7 +17,7 @@ const list = [
     author: 'Dan Abramov, Andrew Clark',
     num_comments: 2,
     points: 5,
-    objectID: 1,
+    objectID: 1
   },
   {
     title: 'Jake Rules',
@@ -25,77 +25,76 @@ const list = [
     author: 'Jake Funke',
     num_comments: 20,
     points: 50,
-    objectID: 2,
+    objectID: 2
   }
 ]
 
 // App extends from Component (inheritance)
 // Components have useful methods like render() etc. Building block for a React app.
 
-function isSearched(searchTerm) {
-  return function(item) {
+function isSearched (searchTerm) {
+  return function (item) {
     // some condition which returns true or false
-    return !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
-
+    return !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase())
   }
 }
 
 class App extends Component {
-
-  constructor(props) {
-    super(props); // mandatory bc it calls constructor of extended Component class.
+  constructor (props) {
+    super(props) // mandatory bc it calls constructor of extended Component class.
 
     this.state = { // initial state is list of items we created above
       list,
-      searchTerm: '',
-    };
+      searchTerm: ''
+    }
 
-    this.onSearchChange = this.onSearchChange.bind(this);
-    this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this)
+    this.onDismiss = this.onDismiss.bind(this)
   }
-  onSearchChange(event) {
+  onSearchChange (event) {
     // define what happens on search change
-    this.setState({ searchTerm: event.target.value });
+    this.setState({ searchTerm: event.target.value })
   }
-  onDismiss(id) {
+  onDismiss (id) {
     // will fill in with a way to remove the clicked item from list
     // will use built in array filter
 
-    const isNotId = item => item.objectID !== id;
-    const updatedList = this.state.list.filter(isNotId);
-    this.setState({ list: updatedList });
+    const isNotId = item => item.objectID !== id
+    const updatedList = this.state.list.filter(isNotId)
+    this.setState({ list: updatedList })
   }
 
-  render() {
+  render () {
     return (
-      <div className="App">
+      <div className='App'>
         <form>
           <input
-            type="text"
+            type='text'
             onChange={this.onSearchChange}
           />
         </form>
 
         { this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span><button
-                      onClick={() => this.onDismiss(item.objectID)}
-                      type = "button"
-                    >
-                    Dismiss
-                    </button>
-              </span>
-            </div>
-          )}
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => this.onDismiss(item.objectID)}
+                type='button'
+              >
+              Dismiss
+            </button>
+            </span>
+          </div>
+        )}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
+  <App />,
+  document.getElementById('root')
+)
 
-registerServiceWorker();
+registerServiceWorker()
 
 // lets react app reload without a page refresh (!)
 // this is amazing.
 if (module.hot) {
-    module.hot.accept()
+  module.hot.accept()
 }
